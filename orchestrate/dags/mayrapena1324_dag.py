b953be62b2e42bbb28368bad3665c0070d594aa3
Mayra Peña <Mayra@datacoves.com>
2024-11-11 15:59:52 -0800

    Add yml dag

--- orchestrate/dags/mayrapena1324_dag.py
+++ orchestrate/dags/mayrapena1324_dag.py
@@ -1,40 +1,42 @@
-from airflow.decorators import dag
+import datetime
+
+from airflow.decorators import dag, task_group
+from airflow.providers.airbyte.operators.airbyte import \
+    AirbyteTriggerSyncOperator
 from operators.datacoves.dbt import DatacovesDbtOperator
-from pendulum import datetime
+
 
 @dag(
     default_args={
-        "start_date": datetime(2023, 10, 10),
-        "owner": "airflow",
-        "email": "mayrapena1324@example.com",
+        "start_date": datetime.datetime(2023, 1, 1, 0, 0),
+        "owner": "Mayra Pena",
+        "email": "mayra@datacoves.com",
         "email_on_failure": True,
     },
-
+    description="Personal Loan Average",
+    schedule_interval="0 0 1 */12 *",
+    tags=["version_1"],
     catchup=False,
-    tags = ["version_1"],
-    description = "My first Airflow DAG",
-
-    # This is a regular CRON schedule. Helpful resources
-    # https://cron-ai.vercel.app/
-    # https://crontab.guru/
-    schedule_interval = "0 0 1 */12 *"
 )
-def mayrapena1324_sample_dag():
+def mayrapena1324_dag():
+    @task_group(group_id="extract_and_load_airbyte", tooltip="Airbyte Extract and Load")
+    def extract_and_load_airbyte():
+        country_populations_datacoves_train = AirbyteTriggerSyncOperator(
+            task_id="country_populations_datacoves_train",
+            connection_id="676575f7-22d7-41f4-ab78-52099d8cbccb",
+            airbyte_conn_id="airbyte_connection",
+        )
+        personal_loans_datacoves_train = AirbyteTriggerSyncOperator(
+            task_id="personal_loans_datacoves_train",
+            connection_id="04b4fc09-bc22-4d19-b457-2d1fe84fbd40",
+            airbyte_conn_id="airbyte_connection",
+        )
 
-    # Calling dbt commands
-    dbt_debug = DatacovesDbtOperator(
-        task_id = "dbt_debug_task",
-        bash_command = "dbt debug",
+    tg_extract_and_load_airbyte = extract_and_load_airbyte()
+    transform = DatacovesDbtOperator(
+        task_id="transform", bash_command="dbt build -s 'tag:daily_run_airbyte+ -t prd'"
     )
-
-    dbt_run = DatacovesDbtOperator(
-        task_id = "dbt_run_task",
-        bash_command = "dbt run --select +mayrapena1324_avg_by_grade",
-    )
-
-    # tells Airflow what order to run tasks in
-    dbt_debug >> dbt_run
+    transform.set_upstream([tg_extract_and_load_airbyte])
 
 
-# Invoke Dag
-dag = mayrapena1324_sample_dag()
+dag = mayrapena1324_dag()
