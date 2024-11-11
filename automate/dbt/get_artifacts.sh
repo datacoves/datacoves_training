c50c4041cb3dd2778468c77f93beaccf329c19f9
Mayra Peña <Mayra@datacoves.com>
2024-11-10 18:14:07 -0800

    Update get_artifacts. fix GA script.

--- automate/dbt/get_artifacts.sh
+++ automate/dbt/get_artifacts.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 
-# Cause script to exit on errors except for controlled checks
+# Cause script to exit on error
 set -e
 
-# Navigate to the DBT project directory
 cd $DATACOVES__DBT_HOME
 
 mkdir -p logs
@@ -12,24 +11,25 @@ dbt run-operation get_last_artifacts
 
 # Check if manifest,son exist, count lines if does or set to 0
 if [ -e "logs/manifest.json" ]; then
-    LINES_IN_MANIFEST="$(grep -c '^' logs/manifest.json)"
+LINES_IN_MANIFEST="$(grep -c '^' logs/manifest.json)"
 else
     LINES_IN_MANIFEST="0"
 fi
 
-if [ $LINES_IN_MANIFEST -eq 0 ]; then
-    echo "Production manifest for dbt was not found in Snowflake."
+if [ $LINES_IN_MANIFEST -eq 0 ]
+then
+    echo "Manifest not found in Snowflake stage, contact the Snowflake administrator to load a updated manifest to snowflake."
     # This is used by github actions
-    # echo "::set-output name=manifest_found::false"
+    echo "::set-output name=manifest_found::false"
 
     # This is used by Jenkins
-    echo "false" >> temp_MANIFEST_FOUND.txt
+    # echo "false" > temp_MANIFEST_FOUND.txt
 else
     echo "Updated manifest from production"
 
     # This is used by github actions
-    # echo "::set-output name=manifest_found::true"
+    echo "::set-output name=manifest_found::true"
 
     # This is used by Jenkins
-    echo "true" >> temp_MANIFEST_FOUND.txt
+    # echo "true" > temp_MANIFEST_FOUND.txt
 fi
