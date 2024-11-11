6483a1f9eaefde4257b1539b39ee6e46e2ec74e0
Mayra Peña <Mayra@datacoves.com>
2024-11-11 10:03:36 -0800

    Debugging

--- automate/dbt/get_artifacts.sh
+++ automate/dbt/get_artifacts.sh
@@ -21,7 +21,7 @@ then
     echo "Manifest not found in Snowflake stage, contact the Snowflake administrator to load a updated manifest to snowflake."
     # This is used by github actions
     # echo "::set-output name=manifest_found::false"
-    echo "{manifest_found}={false}" >> $GITHUB_OUTPUT
+    echo "manifest_found=false" >> $GITHUB_OUTPUT
 
     # This is used by Jenkins
     # echo "false" > temp_MANIFEST_FOUND.txt
@@ -30,7 +30,7 @@ else
 
     # This is used by github actions
     # echo "::set-output name=manifest_found::true"
-    echo "{manifest_found}={true}" >> $GITHUB_OUTPUT
+    echo "manifest_found=true" >> $GITHUB_OUTPUT
 
     # This is used by Jenkins
     # echo "true" > temp_MANIFEST_FOUND.txt
