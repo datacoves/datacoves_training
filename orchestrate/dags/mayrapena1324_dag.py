8e9268820d7f2b787db31b15a90b4f4b474c2a52
Mayra Peña <Mayra@datacoves.com>
2024-11-11 16:58:27 -0800

    Add my first dag

---
+++ orchestrate/dags/mayrapena1324_dag.py
@@ -0,0 +1,42 @@
+import datetime
+
+from airflow.decorators import dag, task_group
+from airflow.providers.airbyte.operators.airbyte import \
+    AirbyteTriggerSyncOperator
+from operators.datacoves.dbt import DatacovesDbtOperator
+
+
+@dag(
+    default_args={
+        "start_date": datetime.datetime(2023, 1, 1, 0, 0),
+        "owner": "Mayra Pena",
+        "email": "mayra@datacoves.com",
+        "email_on_failure": True,
+    },
+    description="Personal Loan Average",
+    schedule_interval="0 0 1 */12 *",
+    tags=["version_1"],
+    catchup=False,
+)
+def mayrapena1324_dag():
+    @task_group(group_id="extract_and_load_airbyte", tooltip="Airbyte Extract and Load")
+    def extract_and_load_airbyte():
+        country_populations_datacoves_train = AirbyteTriggerSyncOperator(
+            task_id="country_populations_datacoves_train",
+            connection_id="676575f7-22d7-41f4-ab78-52099d8cbccb",
+            airbyte_conn_id="airbyte_connection",
+        )
+        personal_loans_datacoves_train = AirbyteTriggerSyncOperator(
+            task_id="personal_loans_datacoves_train",
+            connection_id="04b4fc09-bc22-4d19-b457-2d1fe84fbd40",
+            airbyte_conn_id="airbyte_connection",
+        )
+
+    tg_extract_and_load_airbyte = extract_and_load_airbyte()
+    transform = DatacovesDbtOperator(
+        task_id="transform", bash_command="dbt build -s 'tag:daily_run_airbyte+ -t prd'"
+    )
+    transform.set_upstream([tg_extract_and_load_airbyte])
+
+
+dag = mayrapena1324_dag()
