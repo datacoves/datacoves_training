410edcfa2bd4b0111f953139e7a94eab09ed7274
Mayra Peña <Mayra@datacoves.com>
2024-11-14 12:03:09 -0800

    Fix airflow development

--- orchestrate/dags/sample_dag.py
+++ orchestrate/dags/sample_dag.py
@@ -8,17 +8,20 @@ from operators.datacoves.dbt import DatacovesDbtOperator
     default_args={
         "start_date": datetime.datetime(2023, 1, 1, 0, 0),
         "owner": "Mayra Pena",
-        "email": "mayra@datacoves.com",
+        "email": "mayra@example.com",
         "email_on_failure": True,
+        "retries": 3,
+
     },
     description="Daily dbt run",
-    schedule_interval="0 12 * * *",
+    schedule="0 12 * * *",
     tags=["version_1"],
     catchup=False,
+
 )
 def sample_dag():
     run_dbt = DatacovesDbtOperator(
-        task_id="run_dbt", bash_command="dbt debug"
+        task_id="run_dbt", bash_command="dbt run - s country_codes"
     )
 
 
