f48815c4e2ec22dcf2965f59260826700186086b
Derrick Bovard <Derrick.Bovard@Braunability.com>
2024-11-21 13:42:21 -0800

    Committing DAGs to feature branch.

---
+++ orchestrate/dags/derrickbovard_dag.py
@@ -0,0 +1,43 @@
+import datetime
+
+from airflow.decorators import dag, task_group
+from airflow.providers.airbyte.operators.airbyte import \
+    AirbyteTriggerSyncOperator
+from operators.datacoves.dbt import DatacovesDbtOperator
+
+
+@dag(
+    default_args={
+        "start_date": datetime.datetime(2023, 1, 1, 0, 0),
+        "owner": "Derrick Bovard",
+        "email": "dbovard@example.com",
+        "email_on_failure": True,
+        "retries": 3,
+    },
+    description="Personal Loan Average",
+    schedule="0 0 1 */12 *",
+    tags=["version_1"],
+    catchup=False,
+)
+def derrickbovard_dag():
+    @task_group(group_id="extract_and_load_airbyte", tooltip="Airbyte Extract and Load")
+    def extract_and_load_airbyte():
+        country_populations_datacoves_train = AirbyteTriggerSyncOperator(
+            task_id="country_populations_datacoves_train",
+            connection_id="676575f7-22d7-41f4-ab78-52099d8cbccb",
+            airbyte_conn_id="airbyte_connection",
+        )
+        derrickbovard_source_datacoves_train = AirbyteTriggerSyncOperator(
+            task_id="derrickbovard_source_datacoves_train",
+            connection_id="082728a2-8bbb-4824-a345-ca345cc8313a",
+            airbyte_conn_id="airbyte_connection",
+        )
+
+    tg_extract_and_load_airbyte = extract_and_load_airbyte()
+    transform = DatacovesDbtOperator(
+        task_id="transform", bash_command="dbt build -s 'tag:daily_run_airbyte+ -t prd'"
+    )
+    transform.set_upstream([tg_extract_and_load_airbyte])
+
+
+dag = derrickbovard_dag()
